a829c815c30970a8703b3dbd72291723cc5abf73
Hasmed <samuel.charasse@edu.univ-fcomte.fr>
2025-02-11 10:04:58 +0100

    Ajout de la configuration de la collection "personne" avec un schéma de validation pour les données personnelles.

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const personne = defineCollection({
+  loader: glob({ base: "src/data/personne", pattern: "**/*.md" }),
+  schema: z.object({
+    nom: z.string(),
+    lieuNaissance: z.string(),
+    dateNaissance: z.date(),
+    dateDeces: z.date().optional(),
+    nationalite: z.string(),
+  }),
+});
